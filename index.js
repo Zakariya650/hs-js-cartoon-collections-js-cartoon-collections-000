41dd50b8cc96c6f5c0b725c62fe63c76d4079a2a
Ahmed Mohamud <zakariya650@gmail.com>
2020-01-17 18:50:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function findTheCheese (foods) { var cheeses = ["cheddar", "gouda", "camembert"]
  
  function startswithb(words){
    var wordswithb = []
-   words.find(function)(wordswithb)
+   words.forEach(function(currentword)}{
    if ()
    return startswithb
  }
